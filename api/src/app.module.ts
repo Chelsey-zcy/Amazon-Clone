637bc90d2b2e4de0a6984c5c3393914648ee3028
Chelsey Zhang <chelsey.zcy@gmail.com>
2022-05-13 23:04:24 +1000

    feat: generate user & auth module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,13 +3,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/amazon'),
     ProductModule,
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
