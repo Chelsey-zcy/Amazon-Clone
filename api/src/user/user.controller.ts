c96129b0c068efda8ac8682ecfd5a6b6b61713c6
Chelsey Zhang <chelsey.zcy@gmail.com>
2022-05-16 14:10:30 +1000

    feat: JwtGuard authentication

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserDetails } from './user-details.interface';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Get('id')
+  async getUser(@Param('id') id: string): Promise<UserDetails | null> {
+    return this.userService.findById(id);
+  }
 }
