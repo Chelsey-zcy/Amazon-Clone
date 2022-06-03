0a82595d312e6906aef41f569ccaead44ddedcec
Chelsey Zhang <chelsey.zcy@gmail.com>
2022-06-03 16:13:55 +1000

    feat(homepage): create header & product component

--- amazon/src/App.tsx
+++ amazon/src/App.tsx
@@ -13,6 +13,7 @@ import SigninPage from './pages/Signin.page';
 import PrivateRoute from './features/auth/components/PrivateRoute';
 
 import { store } from './store';
+import { CartPage } from './pages/Cart.page';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<PrivateRoute page={<HomePage />} />} />
+          <Route path="/cart" element={<PrivateRoute page={<CartPage />} />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/signin" element={<SigninPage />} />
           <Route path="*" element={<Navigate to="/" />} />
---
+++ amazon/src/features/products/components/Product.component.tsx
@@ -0,0 +1,70 @@
+import { FC } from 'react';
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from '@mui/material';
+
+import { useAppDispatch, useAppSelector } from '../../../hooks/redux/hooks';
+import { ProductDocument } from '../models/Product';
+import { incrementProduct, decrementProduct } from '../productSlice';
+
+interface ProductComponentProps {
+  product: ProductDocument;
+}
+
+const ProductComponent: FC<ProductComponentProps> = ({ product }) => {
+  const dispatch = useAppDispatch();
+  const { cart } = useAppSelector((state) => state.product);
+  let qty = 0;
+  const cartItem = cart.find((item) => item._id === product._id);
+
+  if (cartItem) {
+    qty = cartItem.quantity;
+  }
+
+  return (
+    <Card sx={{ width: 300, minWidth: 300 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="http://via.placeholder.com/300.png/09f/fff"
+        alt="image"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          ${product.price}
+        </Typography>
+        <Typography gutterBottom variant="body2" color="text.secondary">
+          ${product.description}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Button
+          onClick={() => {
+            dispatch(decrementProduct(product));
+          }}
+          disabled={qty === 0}
+          size="large"
+        >
+          {' '}
+          -{' '}
+        </Button>
+        <span>{qty}</span>
+        <Button
+          onClick={() => {
+            dispatch(incrementProduct(product));
+          }}
+          size="large"
+        >
+          +
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default ProductComponent;
